aad77504de78af8bb3c0fe511990b74f509a863f
My Name <bhardwajharsh0312@gmail.com>
2026-05-01 09:23:44 +0530

    adding programs and tests

---
+++ programs/programs/heliora_prediction_market/src/instructions.rs
@@ -0,0 +1,254 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount};
+use crate::state::Market;
+
+#[derive(Accounts)]
+#[instruction(market_id: u32)]
+pub struct InitializeMarket<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Market::INIT_SPACE,
+        seeds = [b"market", market_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub market: Account<'info, Market>,
+    
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    
+    pub collateral_mint: Account<'info, Mint>,
+    
+    #[account(
+        init,
+        payer = authority,
+        token::mint = collateral_mint,
+        token::authority = market,
+        seeds = [b"vault", market_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub collateral_vault: Account<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = authority,
+        mint::decimals = 6,
+        mint::authority = market,
+        seeds = [b"outcome_a", market_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub outcome_a_mint: Account<'info, Mint>,
+    
+    #[account(
+        init,
+        payer = authority,
+        mint::decimals = 6,
+        mint::authority = market,
+        seeds = [b"outcome_b", market_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub outcome_b_mint: Account<'info, Mint>,
+    
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+
+#[derive(Accounts)]
+#[instruction(market_id: u32)]
+pub struct SplitToken<'info> {
+    #[account(
+        mut,
+        seeds = [b"market", market.market_id.to_le_bytes().as_ref()],
+        bump = market.bump,
+        constraint = market.market_id == market_id
+    )]
+    pub market: Account<'info, Market>,
+    
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = user_collateral.mint == market.collateral_mint,
+        constraint = user_collateral.owner == user.key()
+    )]
+    pub user_collateral: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = collateral_vault.key() == market.collateral_vault
+    )]
+    pub collateral_vault: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = outcome_a_mint.key() == market.outcome_a_mint
+    )]
+    pub outcome_a_mint: Account<'info, Mint>,
+    
+    #[account(
+        mut,
+        constraint = outcome_b_mint.key() == market.outcome_b_mint
+    )]
+    pub outcome_b_mint: Account<'info, Mint>,
+    
+    #[account(
+        mut,
+        constraint = user_outcome_a.mint == market.outcome_a_mint,
+        constraint = user_outcome_a.owner == user.key()
+    )]
+    pub user_outcome_a: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = user_outcome_b.mint == market.outcome_b_mint,
+        constraint = user_outcome_b.owner == user.key()
+    )]
+    pub user_outcome_b: Account<'info, TokenAccount>,
+    
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+#[instruction(market_id:u32)]
+pub struct MergeToken <'info>{
+    #[account(
+        mut,
+        seeds = [b"market", market.market_id.to_le_bytes().as_ref()],
+        bump = market.bump,
+        constraint = market.market_id == market_id
+    )]
+    pub market: Account<'info, Market>,
+   
+   #[account(mut)]
+   pub user:Signer<'info>,
+
+    #[account(
+        mut,
+        constraint = user_collateral.mint == market.collateral_mint,
+        constraint = user_collateral.owner == user.key()
+    )]
+    pub user_collateral: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        constraint = collateral_vault.key() == market.collateral_vault,
+        constraint = collateral_vault.owner == market.key(),
+        constraint = collateral_vault.mint == market.collateral_mint,
+    )]
+    pub collateral_vault: Account<'info, TokenAccount>,
+
+    #[account(
+        constraint = outcome_a_mint.key() == market.outcome_a_mint
+    )]
+    pub outcome_a_mint: Account<'info, Mint>,
+    #[account(
+        constraint = outcome_b_mint.key() == market.outcome_b_mint
+    )]
+    pub outcome_b_mint: Account<'info, Mint>,
+    #[account(
+        mut,
+        constraint = user_outcome_a.mint == market.outcome_a_mint,
+        constraint = user_outcome_a.owner == user.key()
+    )]
+    pub user_outcome_a: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = user_outcome_b.mint == market.outcome_b_mint,
+        constraint = user_outcome_b.owner == user.key()
+    )]
+    pub user_outcome_b: Account<'info, TokenAccount>,
+    
+    pub token_program: Program<'info, Token>,
+
+
+}
+
+#[derive(Accounts)]
+#[instruction(market_id :u32)]
+pub struct SetWinner <'info>{
+
+    #[account(mut)]
+    pub authority : Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"market", market.market_id.to_le_bytes().as_ref()],
+        bump = market.bump,
+        constraint = market.market_id == market_id,
+        constraint = market.authority == authority.key()
+    )]
+    pub market: Account<'info, Market>,
+
+    #[account(
+        mut,
+        constraint = outcome_a_mint.key() == market.outcome_a_mint
+    )]
+    pub outcome_a_mint: Account<'info, Mint>,
+    #[account(
+        mut,
+         constraint = outcome_b_mint.key() == market.outcome_b_mint
+    )]
+    pub outcome_b_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+
+}
+
+#[derive(Accounts)]
+#[instruction(market_id:u32)]
+pub struct ClaimRewards <'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"market", market.market_id.to_le_bytes().as_ref()],
+        bump = market.bump,
+        constraint = market.market_id == market_id
+    )]
+    pub market: Account<'info, Market>,
+
+    #[account(
+        mut,
+        constraint = user_collateral.mint == market.collateral_mint,
+        constraint = user_collateral.owner == user.key()
+    )]
+    pub user_collateral: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = collateral_vault.key() == market.collateral_vault
+    )]
+    pub collateral_vault: Account<'info, TokenAccount>,
+     
+    #[account(
+        mut,
+        constraint = outcome_a_mint.key() == market.outcome_a_mint
+    )]
+    pub outcome_a_mint: Account<'info, Mint>,
+    
+    #[account(
+        mut,
+        constraint = outcome_b_mint.key() == market.outcome_b_mint
+    )]
+    pub outcome_b_mint: Account<'info, Mint>,
+    
+    #[account(
+        mut,
+        constraint = user_outcome_a.mint == market.outcome_a_mint,
+        constraint = user_outcome_a.owner == user.key()
+    )]
+    pub user_outcome_a: Account<'info, TokenAccount>,
+    
+    #[account(
+        mut,
+        constraint = user_outcome_b.mint == market.outcome_b_mint,
+        constraint = user_outcome_b.owner == user.key()
+    )]
+    pub user_outcome_b: Account<'info, TokenAccount>,
+    
+    pub token_program: Program<'info, Token>,
+ 
+}
